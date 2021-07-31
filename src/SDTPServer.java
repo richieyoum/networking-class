1fb25410ee04a80a639a30e63afe0f5625aaaa07
richie youmls <seongmin.youm@mail.mcgill.ca>
2021-07-31 18:32:27 -0400

    refractored SDTPServer

--- src/SDTPServer.java
+++ src/SDTPServer.java
@@ -4,47 +4,76 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class SDTPServer {
-    private ServerSocket serverSocket;
 
-    public void start(int port){
-        try{
-            serverSocket = new ServerSocket(port);
-            while (true){
+    /**
+     * Starts TCP connection and listens to the port
+     * creates server socket and continuously initiate handler objects that handles multiple clients.
+     *
+     * @param port port to bind to
+     */
+    public void start(String port) {
+        try {
+            ServerSocket serverSocket = new ServerSocket(Integer.parseInt(port));
+            while (true) {
                 new ClientHandler(serverSocket.accept()).start();
             }
-        } catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    private static class ClientHandler extends Thread{
+    /**
+     * Handles client requests in a thread
+     */
+    private static class ClientHandler extends Thread {
         private Socket clientSocket;
         private PrintWriter out;
         private BufferedReader in;
+        // session indicator
         private boolean session = false;
 
-        public ClientHandler(Socket socket){
+        /**
+         * Constructor for ClientHandler
+         *
+         * @param socket client socket to handle
+         */
+        public ClientHandler(Socket socket) {
             this.clientSocket = socket;
         }
 
-        public void run(){
+        /**
+         * Handles server commands
+         */
+        public void run() {
             try {
+                // out writer to the client
                 out = new PrintWriter(clientSocket.getOutputStream(), true);
+                // input reader from client
                 in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                // send greeting msg
                 out.println("HELLO CCCS 431 SDTP Server written by Richie Youm READY");
                 out.println("Hit enter to STOP.\n");
                 String msg = "";
                 while (!msg.equals("BYE")) {
+                    // read request from client
                     msg = in.readLine();
+                    // capture current datetime
                     LocalDateTime now = LocalDateTime.now();
+                    // connection initiation request
                     if (msg.equals("HELLO")) {
-                        if (session){
+                        // print error if already connected
+                        if (session) {
                             out.println("ERROR Already in session.\n");
-                        } else{
+                        }
+                        // otherwise, indicate connection success and client address
+                        else {
                             out.println("ALOHA " + clientSocket.getRemoteSocketAddress() + "\n");
+                            // client is now connected to session
                             session = true;
                         }
-                    } else if (msg.equals("HELP")){
+                    }
+                    // help command
+                    else if (msg.equals("HELP")) {
                         out.println("<SDTP Server Commands>");
                         out.println("\tHELP: Get description of available commands");
                         out.println("\tDOW: get date of week. Must initiate session with HELLO command first");
@@ -54,47 +83,67 @@ public class SDTPServer {
                                 "whitespace after the command. Must initiate session with HELLO command first");
                         out.println("\tBYE (<Enter>): terminate the connection");
                         out.println(".\n");
-                    } else if (msg.equals("DOW")) {
-                        if (!session){
+                    }
+                    // date of week command
+                    else if (msg.equals("DOW")) {
+                        // must be connected to session first
+                        if (!session) {
                             out.println("ERROR Not in session. Run HELLO to connect to a session.\n");
-                        } else{
+                        } else {
+                            // first 3 letter of the DOW only
                             out.println(now.getDayOfWeek().toString().substring(0, 3) + "\n");
                         }
-                    } else if (msg.equals("TIME")){
-                        if (!session){
+                    }
+                    // time command
+                    else if (msg.equals("TIME")) {
+                        // must be connected to session first
+                        if (!session) {
                             out.println("ERROR Not in session. Run HELLO to connect to a session.\n");
                         } else {
                             out.println(now.toLocalTime() + "\n");
                         }
-                    } else if (msg.equals("DATE")){
-                        if (!session){
+                    }
+                    // date command
+                    else if (msg.equals("DATE")) {
+                        // must be connected to session first
+                        if (!session) {
                             out.println("ERROR Not in session. Run HELLO to connect to a session.\n");
                         } else {
                             out.println(now.toLocalDate() + "\n");
                         }
-                    } else if (msg.contains("DATETIME")){
-                      if (!session){
-                          out.println("ERROR Not in session. Run HELLO to connect to a session.\n");
-                      } else{
-                        if (msg.strip().length() > 8){
-                            // Treat everything within the line that come after "DATETIME" command as the format.
-                            String format = msg.split(" ", 2)[1];
-                            try{
-                                DateTimeFormatter formatter = DateTimeFormatter.ofPattern(format);
-                                String formattedDate = now.format(formatter);
-                                out.println(formattedDate + "\n");
-                            } catch (Exception e){
+                    }
+                    // datetime command
+                    else if (msg.contains("DATETIME")) {
+                        // must be connected to session first
+                        if (!session) {
+                            out.println("ERROR Not in session. Run HELLO to connect to a session.\n");
+                        } else {
+                            // if there's any additional command that follows DATETIME, use it as the date format
+                            if (msg.strip().length() > "DATETIME".length()) {
+                                String format = msg.split(" ", 2)[1];
+                                try {
+                                    DateTimeFormatter formatter = DateTimeFormatter.ofPattern(format);
+                                    String formattedDate = now.format(formatter);
+                                    out.println(formattedDate + "\n");
+                                } catch (Exception e) {
+                                    out.println(now + "\n");
+                                }
+                            } else {
                                 out.println(now + "\n");
                             }
-                        } else{
-                            out.println(now + "\n");
                         }
-                      }
-                    } else if (msg.equals("BYE") || msg.length()==0) {
+                    }
+                    // exit (BYE) command
+                    else if (msg.equals("BYE") || msg.length() == 0) {
                         out.println("BYE\n");
+                        // terminate connection
                         stopConnection();
-                    } else {
+                    }
+                    // in the case of invalid commands, show error and terminate connection
+                    else {
                         out.println("Unknown command: " + msg + "\n");
+                        // terminate connection
+                        stopConnection();
                     }
                 }
             } catch (IOException e) {
@@ -102,25 +151,33 @@ public class SDTPServer {
             }
         }
 
-        public void stopConnection() throws IOException {
-            in.close();
-            out.close();
-            clientSocket.close();
-        }
-
-        public static void main(String[] args){
-            String ip="127.0.0.1";
-            // Default port, but 431 requires sudo execution.
-            int port=431;
-            try{
-                ip = args[0];
-                port = Integer.parseInt(args[1]);
-            } catch(Exception e){
+        /**
+         * Terminates connection with the client
+         */
+        public void stopConnection() {
+            try {
+                session = false;
+                in.close();
+                out.close();
+                clientSocket.close();
+            } catch (Exception e) {
                 e.printStackTrace();
             }
-            SDTPServer server = new SDTPServer();
-            server.start(port);
         }
 
+        public static void main(String[] args) {
+            // Default port. Since port 431 requires sudo execution, using 4444 instead.
+            String port = "4444";
+            // If port is provided in the arg, use it instead.
+            try {
+                port = args[1];
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            // initialize the server
+            SDTPServer server = new SDTPServer();
+            // start the server and bind to the port
+            server.start(port);
+        }
     }
 }
