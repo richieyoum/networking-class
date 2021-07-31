79f7fd1e7a0910ab3e7c2971901fd92d60776f3a
richie youmls <seongmin.youm@mail.mcgill.ca>
2021-07-31 18:33:47 -0400

    refractored SDTPClient

--- src/SDTPClient.java
+++ src/SDTPClient.java
@@ -6,73 +6,67 @@ public class SDTPClient {
     private PrintWriter out;
     private BufferedReader in;
 
-    public SDTPClient(String ip, int port){
-        try{
-            clientSocket = new Socket(ip, port);
+    public SDTPClient(String ip, String port) {
+        try {
+            clientSocket = new Socket(ip, Integer.parseInt(port));
             in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
             out = new PrintWriter(clientSocket.getOutputStream(), true);
             // Print greeting msg from server
             String res;
-            while(!(res=in.readLine()).equals("")){
+            while (!(res = in.readLine()).equals("")) {
                 System.out.println(res);
             }
-        } catch (UnknownHostException e){
-            e.printStackTrace();
-        } catch (IOException e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    public static boolean sendCommand(SDTPClient client, String msg){
-        try{
-            if (msg.equals("server help")){
-                client.send("HELP");
-            } else if (msg.equals("help")){
-                System.out.println("<SDTP Client Commands>");
-                System.out.println("\thelp: get help commands on the client end");
-                System.out.println("\tserver help: get help commands on the server end");
-                System.out.println("\tconnect: connect to the SDTP server");
-                System.out.println("\tget DOW: get date of week. Must connect to server first");
-                System.out.println("\tget time: get current time. Must connect to server first");
-                System.out.println("\tget date: get current date. Must connect to server first");
-                System.out.println("\tget datetime: get current date and time. Must connect to server first");
-                System.out.println("\texit: terminate the connection");
-            } else if (msg.equals("connect")){
-                client.send("HELLO");
-            } else if (msg.equals("get DOW")){
-                client.send("DOW");
-            } else if (msg.equals("get time")){
-                client.send("TIME");
-            } else if (msg.equals("get date")){
-                client.send("DATE");
-            } else if (msg.strip().contains("get datetime")){
-                if (msg.length() > "get datetime".length()){
-                    try{
-                        String format = msg.replace("get datetime ", "");
-                        client.send("DATETIME " + format);
-                    } catch (Exception e){
-                        client.send("DATETIME");
-                        e.printStackTrace();
-                    }
-                } else{
-                    client.send("DATETIME");
+    public static String convertToCommand(String msg) {
+        if (msg.equals("server help") || msg.equals("HELP")) {
+            return "HELP";
+        } else if (msg.equals("help")) {
+            System.out.println("<SDTP Client Commands>");
+            System.out.println("\thelp: get help commands on the client end");
+            System.out.println("\tserver help: get help commands on the server end. Note, you can't directly use" +
+                    "these server commands here! You need to execute it in something like a telnet connection.");
+            System.out.println("\tconnect: connect to the SDTP server");
+            System.out.println("\tget DOW: get date of week. Must connect to server first");
+            System.out.println("\tget time: get current time. Must connect to server first");
+            System.out.println("\tget date: get current date. Must connect to server first");
+            System.out.println("\tget datetime: get current date and time. Must connect to server first");
+            System.out.println("\texit: terminate the connection");
+            return "";
+        } else if (msg.equals("connect")) {
+            return "HELLO";
+        } else if (msg.equals("get DOW")) {
+            return "DOW";
+        } else if (msg.equals("get time")) {
+            return "TIME";
+        } else if (msg.equals("get date")) {
+            return "DATE";
+        } else if (msg.strip().contains("get datetime")) {
+            if (msg.length() > "get datetime".length()) {
+                try {
+                    String format = msg.replace("get datetime ", "");
+                    return "DATETIME " + format;
+                } catch (Exception e) {
+                    return "DATETIME";
                 }
-            } else if (msg.equals("exit") | msg.equals("BYE")){
-                client.send("BYE");
-                return true;
-            } else{
-                client.send(msg);
+            } else {
+                return "DATETIME";
             }
-        } catch (IOException e){
-            e.printStackTrace();
+        } else if (msg.equals("exit")) {
+            return "BYE";
+        } else {
+            System.out.println("Unknown command: " + msg);
+            return "";
         }
-        return false;
     }
 
     public void send(String msg) throws IOException {
         out.println(msg);
         String res;
-        while (!(res=in.readLine()).equals("") && !res.equals('.')){
+        while (!(res = in.readLine()).equals("")) {
             System.out.println(res);
         }
     }
@@ -83,27 +77,65 @@ public class SDTPClient {
         client.clientSocket.close();
     }
 
-    public static void main(String[] args) throws IOException {
-        String ip="127.0.0.1";
-        // Default port, but 431 requires sudo execution.
-        int port=431;
+    public static void main(String[] args) {
+        // default IP and port
+        String ip = "127.0.0.1";
+        String port = "431";
+        SDTPClient client = null;
 
-        try{
-            ip = args[0];
-            port = Integer.parseInt(args[1]);
-        } catch(Exception e){
+        // continuously accept user input until termination command
+        try {
+            label:
+            while (true) {
+                BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+                String msg = br.readLine();
+                String command = convertToCommand(msg);
+
+                // Connection request prompts user for ip and port, then initialize the client and send HELLO to server.
+                switch (command) {
+                    case "HELLO":
+                        // close current connection if exists
+                        if (client != null) {
+                            stopConnection(client);
+                        }
+                        // prompt user for ip and port of the server
+                        try {
+                            System.out.println("Enter the ip address");
+                            ip = br.readLine();
+                            System.out.println("Enter the port");
+                            port = br.readLine();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        client = new SDTPClient(ip, port);
+                        client.send(command);
+                        break;
+                    // termination request
+                    case "BYE":
+                        // terminate connection if already connected. Otherwise, just exit.
+                        if (client != null) {
+                            client.send(command);
+                            stopConnection(client);
+                        }
+                        break label;
+
+                    // empty output (ie- help)
+                    case "":
+                        // do nothing when command empty
+                        break;
+                    // the rest of commands are sent to the server
+                    default:
+                        if (client != null) {
+                            client.send(command);
+                        } else {
+                            System.out.println("ERROR Not in session. Run connect command to connect to a session.");
+                        }
+                        break;
+                }
+            }
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
-        SDTPClient client = new SDTPClient(ip, port);
-        while (true){
-            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-            String line = br.readLine();
-            boolean terminate = sendCommand(client, line);
-            if (terminate){
-                stopConnection(client);
-                break;
-            }
-        }
     }
 }
